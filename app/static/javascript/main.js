da96be621aaa403ee164ae5b7c16932f60a64901
Stacey Kenny <stacey.kenny@code.berlin>
2022-04-26 18:18:50 +0200

    test images, js mouseover, svgs added

---
+++ app/static/javascript/main.js
@@ -0,0 +1,23 @@
+// document.getElementById("test-js").innerHTML = "Testing JS";
+
+function jsTest2(image) {
+  image.src = "static/images/Kazakhstan.jpeg";
+}
+
+// kazakhstan
+function KazakImage1(image) {
+  image.src = "static/images/Kazakhstan.jpeg";
+}
+
+function KazakImage2(image) {
+  image.src = "static/images/Kazakhstan-2.jpeg";
+}
+
+// alge
+function algeImage1(image) {
+  image.src = "static/images/Algeria-2.jpeg";
+}
+
+function algeImage2(image) {
+  image.src = "static/images/Algeria.jpeg";
+}
